60ab08a42a77d1c704024c6be87cc33809a24be5
Ryota9 <ngm9dwf@gmail.com>
2022-06-03 23:44:47 +0900

    エラー修正

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,10 @@
 require_relative 'boot'
-
 require 'rails/all'
+require 'devise'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 if Rails.env.development? || Rails.env.test?
   Bundler.require(*Rails.groups)
